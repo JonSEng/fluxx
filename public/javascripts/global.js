a27e67593f09823899bca259dad4d2198d8048fb
Jonathan Eng <jwsyeng@gmail.com>
2015-04-29 22:06:52 -0700

    Delete works and new user works too!

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -3,6 +3,8 @@ var userListData = [];
 $(document).ready(function() {
     populateTable();
     $('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo);
+    $('#btnAddUser').on('click', addUser);
+    $('#userList table tbody').on('click', 'td a.linkdeleteuser', deleteUser);
 })
 
 function populateTable() {
@@ -21,24 +23,77 @@ function populateTable() {
 }
 
 function showUserInfo(event) {
-    console.log("in showUserInfo")
-    window.e1 = event
-    window.this1 = $(this)
     event.preventDefault();
 
     var thisUserName = $(this).attr('rel');
-    console.log(thisUserName);
 
     var arrayPosition = userListData.map(function(arrayItem) { 
         return arrayItem.username; 
     }).indexOf(thisUserName);
-    console.log(arrayPosition)
 
     var thisUserObject = userListData[arrayPosition];
-    console.log(thisUserObject)
 
     $('#userInfoName').text(thisUserObject.fullname);
     $('#userInfoAge').text(thisUserObject.age);
     $('#userInfoGender').text(thisUserObject.gender);
     $('#userInfoLocation').text(thisUserObject.location);
+}
+
+function addUser(event) {
+    event.preventDefault();
+
+    var errorCount = 0;
+    $('#addUser input').each(function(index, val) {
+        if ($(this).val() === '') { errorCount++; } ;
+    });
+
+    if (errorCount === 0) {
+        var newUser = {
+            'username': $('#addUser fieldset input#inputUserName').val(),
+            'email': $('#addUser fieldset input#inputUserEmail').val(),
+            'fullname': $('#addUser fieldset input#inputUserFullname').val(),
+            'age': $('#addUser fieldset input#inputUserAge').val(),
+            'location': $('#addUser fieldset input#inputUserLocation').val(),
+            'gender': $('#addUser fieldset input#inputUserGender').val()
+        }
+
+        $.ajax({
+            type: 'POST',
+            data: newUser,
+            url: '/users/adduser',
+            dataType: 'JSON'
+        }).done(function (response) {
+            if (response.msg === '') {
+                $('#addUser fieldset input').val('');
+                populateTable();
+            }
+            else {
+                alert('Error ' + response.msg);
+            }
+        })
+    } else {
+        alert('Errorcount = ' + errorCount);
+        return false;
+    }
+}
+
+function deleteUser(event) {
+    event.preventDefault();
+
+    var confirmation = confirm('Are you sure you want to delete this user?');
+    if (confirmation === true) {
+        $.ajax({
+            type:'DELETE',
+            url: '/users/deleteuser/' + $(this).attr('rel')
+        }).done(function (response) {
+            if (response.msg === '') {
+
+            }  else {
+                alert('Error: ' + response.msg);
+            }         
+            populateTable();
+        });
+    } else {
+        return false;
+    }
 }
