47da2459c396d34bd76d905fffcbd58311ba58a7
Jonathan Eng <jwsyeng@gmail.com>
2015-05-06 22:40:57 -0700

    Has a progress dashboard!

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var d3 = require("d3");
 
 var fluxx = require('./routes/fluxx');
 
