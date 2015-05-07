1130861f78d8fbb06cc14cf29bb2d66c1a81b6c9
Jonathan Eng <jwsyeng@gmail.com>
2015-05-06 21:08:28 -0700

    Added a progress page (clean and blank for now)

--- routes/fluxx.js
+++ routes/fluxx.js
@@ -3,13 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log("fluxx router")
+  console.log("fluxx router");
   res.render('fluxx_view', { title: 'fluxx home' });
 });
 
 router.get('/tasks', function(req, res, next) {
-  console.log("tasks router")
+  console.log("tasks router");
   res.render('tasks', { title: 'tasks' });
+});
+
+router.get('/progress', function(req, res, next) {
+  console.log("routing to progress!");
+  res.render('progress', { title: 'Superhero Progress' });
 });
 
 module.exports = router;
